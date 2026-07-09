4f19a966bd8fa27069dc2d7c7c537d37933007b3
godspeed-oss <626490577@qq.com>
2026-07-09 23:04:35 +0800

    manage devices with ArrayList

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Device device1 = new Device(1, "显微镜", "光学设备", "可预约");
-        Device device2 = new Device(2, "3D打印机", "制造设备", "维修中");
+        ArrayList<Device> devices = new ArrayList<>();
 
-        device1.printInfo();
-        device2.printInfo();
+        devices.add(new Device(1, "显微镜", "光学设备", "可预约"));
+        devices.add(new Device(2, "3D打印机", "制造设备", "维修中"));
+        devices.add(new Device(3, "高速离心机", "实验设备", "可预约"));
+
+        System.out.println("实验室设备列表：");
+        System.out.println("--------------------");
+
+        for (Device device : devices) {
+            device.printInfo();
+        }
     }
 }
