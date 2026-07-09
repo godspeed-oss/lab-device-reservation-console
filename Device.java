1cbe671e58817bf7c60fe98356036904197f780d
godspeed-oss <626490577@qq.com>
2026-07-09 22:57:50 +0800

    add Device class

---
+++ Device.java
@@ -0,0 +1,37 @@
+public class Device {
+    private int id;
+    private String name;
+    private String type;
+    private String status;
+
+    public Device(int id, String name, String type, String status) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void printInfo() {
+        System.out.println("设备编号：" + id);
+        System.out.println("设备名称：" + name);
+        System.out.println("设备类型：" + type);
+        System.out.println("设备状态：" + status);
+        System.out.println("--------------------");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lab Device Reservation Console");
+        Device device1 = new Device(1, "显微镜", "光学设备", "可预约");
+        Device device2 = new Device(2, "3D打印机", "制造设备", "维修中");
+
+        device1.printInfo();
+        device2.printInfo();
     }
 }
