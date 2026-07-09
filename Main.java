0492d84aedbfc69449cda7e10ed2e9b65087105c
godspeed-oss <626490577@qq.com>
2026-07-09 23:06:25 +0800

    add console menu

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,33 @@ public class Main {
         devices.add(new Device(2, "3D打印机", "制造设备", "维修中"));
         devices.add(new Device(3, "高速离心机", "实验设备", "可预约"));
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("实验室设备预约系统");
+            System.out.println("1. 查看设备列表");
+            System.out.println("0. 退出系统");
+            System.out.print("请输入功能编号：");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    showDevices(devices);
+                    break;
+                case 0:
+                    System.out.println("系统已退出");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("输入错误，请重新输入");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void showDevices(ArrayList<Device> devices) {
         System.out.println("实验室设备列表：");
         System.out.println("--------------------");
 
